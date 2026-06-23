f4191dd1ffc191270c1d7b348914cd4ae4d8e167
dukaludic <dukaludic@gmail.com>
2026-06-23 16:02:50 +0200

    feat: normalize RSS1, RSS2 and Atom feed types

--- src/lib/feeds/parser.ts
+++ src/lib/feeds/parser.ts
@@ -1,13 +1,30 @@
-import Parser from "rss-parser";
-import { ParsedFeed, ParsedFeedItem } from "./types";
-import { fetchFeed } from "./fetch";
+import { ParsedFeed, RSS2Feed, RSS2FeedItem, RSS1Feed, RSS1FeedItem, AtomFeed, AtomEntry } from "./types";
+import { XMLParser } from "fast-xml-parser";
+import util from "util";
 
 export async function parseFeed(feed: string): Promise<ParsedFeed | null> {
-    const parser = new Parser();
 
     try {
-        const json = await parser.parseString(feed);
-        return normalizeFeed(json);
+        const type = detectRSSTypeFromXML(feed);
+
+        const parser = new XMLParser({
+            ignoreAttributes: false
+        });
+
+        const json = parser.parse(feed);
+
+        switch (type) {
+            case "rss2":
+                return normalizeRSS2Feed(json);
+            case "atom":
+                return normalizeAtomFeed(json);
+            case "rss1":
+                return normalizeRSS1Feed(json);
+            default:
+                console.error(`Unknown RSS type: ${type}`);
+                return null;
+        }
+
     } catch (error) {
         if (error instanceof Error && error.message.includes('404')) {
             console.error(error);
@@ -24,22 +41,75 @@ export async function parseFeed(feed: string): Promise<ParsedFeed | null> {
     }
 }
 
-function normalizeFeed(feed: Parser.Output<Parser.Item>): ParsedFeed {
+function detectRSSTypeFromXML(xml: string) {
+    const json = new XMLParser().parse(xml);
+
+    if (json.rss?.channel) return "rss2";
+    if (json.feed) return "atom";
+    if (json["rdf:RDF"]) return "rss1";
+    return "unknown";
+}
+
+function normalizeRSS2Feed(feed: RSS2Feed): ParsedFeed | null {
+    const items = Array.isArray(feed.rss.channel.item) ? feed.rss.channel.item ?? [] : [feed.rss.channel.item];
+
     return {
         feed: {
-            title: feed.title ?? '',
-            url: feed.link ?? '',
+            title: feed.rss.channel.title ?? '',
+            url: feed.rss.channel.link ?? '',
         },
-        items: [feed.items.map((item: Record<string, any>) => ({
-            title: item.title ?? 'Untitled',
-            url: item.link ?? 'No URL',
-            description: item.content ?? item.contentSnippet ?? item.summary ?? '',
-            published_at: new Date(item.pubDate ?? ''),
-        }))[0]],
+        items: items.map((item: RSS2FeedItem) => {
+            const publishedAt = item.pubDate ? new Date(item.pubDate) : new Date();
+            return {
+                title: item.title ?? 'Untitled',
+                url: item.link ?? 'No URL',
+                description: item.description ?? '',
+                published_at: publishedAt,
+            }
+        }) ?? [],
     }
 }
 
+function normalizeRSS1Feed(feed: RSS1Feed): ParsedFeed | null {
+    const items = Array.isArray(feed["rdf:RDF"].item) ? feed["rdf:RDF"].item ?? [] : [feed["rdf:RDF"].item];
 
-parseFeed(await fetchFeed("https://www.reddit.com/") ?? "").then((result) => {
-    console.log(result);
-});
+    return {
+        feed: {
+            title: feed["rdf:RDF"].channel.title,
+            url: feed["rdf:RDF"].channel.link,
+        },
+        items: items.map((item: RSS1FeedItem) => {
+            const publishedAt = item['dc:date'] ? new Date(item['dc:date']) : new Date();
+            return {
+                title: item.title,
+                url: item.link,
+                description: item.description,
+                published_at: publishedAt,
+            }
+        }) ?? [],
+    }
+}
+
+function normalizeAtomFeed(feed: AtomFeed): ParsedFeed | null {
+
+    console.log(util.inspect(feed, { depth: null }));
+
+    const feedUrl = Array.isArray(feed.feed.link) ? feed.feed.link[0] : feed.feed.link;
+    const feedEntries = Array.isArray(feed.feed.entry) ? feed.feed.entry : [feed.feed.entry];
+
+    return {
+        feed: {
+            title: feed.feed.title,
+            url: feedUrl['@_href'] ?? '',
+        },
+        items: feedEntries.map((item: AtomEntry) => {
+            const link = Array.isArray(item.link) ? item.link[0] : item.link;
+            return {
+                title: item.title,
+                url: link['@_href'],
+                description: item.summary ?? item.content,
+                published_at: new Date(item.updated),
+            }
+        }) ?? [],
+    }
+}
--- src/lib/feeds/types.ts
+++ src/lib/feeds/types.ts
@@ -1,14 +1,105 @@
 export interface ParsedFeedItem {
-    title: string;
+    title?: string;
     url?: string;
-    description: string;
+    description?: string;
     published_at: Date;
 }
 
 export interface ParsedFeed {
     feed: {
-        title: string;
-        url: string;
+        title?: string;
+        url?: string;
     };
     items: ParsedFeedItem[];
+}
+
+
+export interface RSS2Feed {
+    rss: {
+        channel: {
+            title: string;
+            link: string;
+            description: string;
+            item: RSS2FeedItem[];
+        }
+    }
+}
+
+export interface RSS2FeedItem {
+    title: string;
+    link: string;
+    description: string;
+    pubDate: string;
+}
+
+export interface RSS1Feed {
+    "rdf:RDF": RDFRoot;
+}
+
+export interface RDFRoot {
+    "@_xmlns:rdf": string;
+    "@_xmlns": string;
+
+    channel: RSS1Channel;
+    item: RSS1FeedItem[];
+}
+
+export interface RSS1Channel {
+    "@_rdf:about"?: string;
+
+    title: string;
+    link: string;
+    description?: string;
+
+    items?: {
+        "rdf:Seq": {
+            "rdf:li": Array<{
+                "@_rdf:resource": string;
+            }>;
+        };
+    };
+}
+
+export interface RSS1FeedItem {
+    "@_rdf:about"?: string;
+
+    title?: string;
+    link?: string;
+    description?: string;
+    "dc:date"?: string;
+}
+
+export interface AtomFeed {
+    feed: AtomFeedRoot;
+}
+
+export interface AtomFeedRoot {
+    id: string;
+    title: string;
+    updated: string;
+    link: AtomLink | AtomLink[];
+    author?: AtomPerson | AtomPerson[];
+    subtitle?: string;
+    entry: AtomEntry[];
+}
+
+export interface AtomLink {
+    ['@_href']: string;
+    ['@_rel']: "alternate" | "self" | string;
+}
+
+export interface AtomPerson {
+    name?: string;
+    email?: string;
+    uri?: string;
+}
+
+export interface AtomEntry {
+    id: string;
+    title?: string;
+    updated: string;
+    link: AtomLink | AtomLink[];
+    author?: AtomPerson | AtomPerson[];
+    content?: string;
+    summary?: string;
 }
